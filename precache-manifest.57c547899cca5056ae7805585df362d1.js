e453ce349698dbf752e14afae866909ce007d307
PBscoots <vanderpool.andrew@gmail.com>
2020-01-20 15:48:35 +0100

    Updates

---
+++ precache-manifest.57c547899cca5056ae7805585df362d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f94f7dac07fd6f0b519db9041a0162",
+    "url": "/circlepunch/index.html"
+  },
+  {
+    "revision": "07642d54a4c0d3328058",
+    "url": "/circlepunch/static/css/main.023116ab.chunk.css"
+  },
+  {
+    "revision": "a4cf561f1312e07bd517",
+    "url": "/circlepunch/static/js/2.1d3c51a7.chunk.js"
+  },
+  {
+    "revision": "07642d54a4c0d3328058",
+    "url": "/circlepunch/static/js/main.71a15155.chunk.js"
+  },
+  {
+    "revision": "ed0660b49f44325e7352",
+    "url": "/circlepunch/static/js/runtime-main.2fcd1682.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/circlepunch/static/media/logo.25bf045c.svg"
+  }
+]);
